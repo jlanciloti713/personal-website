5e8af40c78466ba4be367d5465831836d0d5d048
Jack Lanciloti <jlanciloti713@gmail.com>
2018-10-06 17:32:42 -0500

    Create changes to personal-website

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+ get "/" => 'static_pages#about'
  get "/about" => 'static_pages#about'
  get "/contact" => 'static_pages#contact'
  get "/portfolio" => 'static_pages#portfolio'
